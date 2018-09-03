78819b35e27c82de69e4d23895ba468f409deda5
Mujina <mujina@pugme.co.uk>
2018-09-03 22:21:06 +0100

    Implement pytest checks

--- palindrome.py
+++ palindrome.py
@@ -1,11 +1,43 @@
 #!/usr/bin/env python
 import sys
+import logging
 
-def is_palindrome(p):
-  if p == "".join(reversed(p)): 
-    print("Palindrome")
-  else:
-    print("Not")
+logger = logging.getLogger('ft_palindrome')
+logger.setLevel(logging.INFO)
+screen_handler = logging.StreamHandler(stream=sys.stdout)
+logger.addHandler(screen_handler)
+
+class ft_palindrome:
+
+    def __init(self):
+        pass
+
+    def is_palindrome(self, p):
+
+        if not p:
+            raise TypeError("Argument cannot be blank")
+
+        try:
+            i_am_int = isinstance(int(p), int)
+        except ValueError:
+            pass
+        else:
+            raise TypeError("Argument cannot be an integer")
+
+        if not isinstance(p, str):
+            raise TypeError("Argument must be a string.")
+        if p == "".join(reversed(p)):
+            return("Palindrome")
+        else:
+            return("Not")
 
 if __name__ == "__main__":
-  is_palindrome(sys.argv[1])
+  ft_palindrome = ft_palindrome()
+  arg = None
+  try:
+    arg = sys.argv[1]
+  except IndexError:
+    pass
+  ans = ft_palindrome.is_palindrome(arg)
+  print(ans)
+
---
+++ test_palindrome.py
@@ -0,0 +1,32 @@
+import pytest
+from typing import Any
+
+from palindrome import ft_palindrome
+
+@pytest.fixture
+def palindrome():
+    return ft_palindrome()
+
+def test_is_palindrome(palindrome):
+    test_arg = 'abba'  # type: String
+    assert palindrome.is_palindrome(test_arg) == "Palindrome"
+
+def test_is_not_a_palindrome(palindrome):
+    test_arg = 'abcd'
+    assert palindrome.is_palindrome(test_arg) == "Not"
+
+def test_number_arg_raises_value_error(palindrome):
+    with pytest.raises(TypeError):
+        test_arg = 12321
+        palindrome.is_palindrome(test_arg)
+
+def test_other_arg_type_raises_type_error(palindrome):
+    with pytest.raises(TypeError):
+        test_arg = []
+        palindrome.is_palindrome(test_arg)
+
+def test_no_arg_type_raises_type_error(palindrome):
+    with pytest.raises(TypeError):
+        test_arg = None
+        palindrome.is_palindrome(test_arg)
+
